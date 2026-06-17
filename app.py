2619c3b1e9c7d4dd27c86bd4ff1a490596ff95eb
pirtikshas-coder <pirtikshasekar0503@gmail.com>
2026-06-17 18:02:30 +0000

    Version 2 Professional Dashboard

--- app.py
+++ app.py
@@ -1,25 +1,69 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
 def generate_ai_response(query):
-    return f"AI Research Summary\n\nTopic: {query}\n\nThis is a demo AI response."
+
+    return f"""
+Topic:
+{query}
+
+Summary:
+{query} is an emerging research field.
+
+Applications:
+• Healthcare
+• Industry
+• Education
+• Artificial Intelligence
+
+Future Scope:
+Strong growth expected.
+"""
 
 @app.route("/")
 def home():
-    return render_template("index.html")
 
-@app.route("/search", methods=["POST"])
+    return render_template(
+        "index.html"
+    )
+
+@app.route(
+"/search",
+methods=["POST"]
+)
+
 def search():
-    data = request.json
-    query = data.get("query")
 
-    response = generate_ai_response(query)
+    data = request.json
+
+    query = data.get(
+        "query"
+    )
+
+    result = generate_ai_response(
+        query
+    )
 
     return jsonify({
-        "result": response
+
+        "result":
+        result
+
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run(
+
+        host="0.0.0.0",
+
+        port=5000,
+
+        debug=True
+
+    )
     
--- static/script.js
+++ static/script.js
@@ -1,18 +1,66 @@
 function searchAI(){
 
-    let query = document.getElementById("query").value;
+let query =
 
-    fetch("/search",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            query:query
-        })
-    })
-    .then(response=>response.json())
-    .then(data=>{
-        document.getElementById("resultBox").innerText=data.result;
-    });
+document
+.getElementById(
+"query"
+)
+.value;
+
+fetch(
+"/search",
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":
+
+"application/json"
+
+},
+
+body:
+
+JSON.stringify({
+
+query:query
+
+})
+
+}
+
+)
+
+.then(
+
+response=>
+
+response.json()
+
+)
+
+.then(
+
+data=>{
+
+document
+
+.getElementById(
+
+"resultBox"
+
+)
+
+.innerHTML=
+
+data.result;
+
+}
+
+);
+
 }
